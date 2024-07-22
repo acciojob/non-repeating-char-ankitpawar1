e2db1cc52e76769d081e1dcdeb67e4672178286f
ankitpawar1 <54860507+ankitpawar1@users.noreply.github.com>
2024-07-22 12:39:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    const charCount = {};
+
+    for (let char of str) {
+        if (charCount[char]) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
+    }
+
+    for (let char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+    return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+function findAndDisplayFirstNonRepeatedChar() {
+    const str = document.getElementById('stringInput').value;
+    const result = firstNonRepeatedChar(str);
+    document.getElementById('result').innerText = result === null ? 'null' : result;
+}
+
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input "aabbcdd", uncomment the following line
+// console.log(firstNonRepeatedChar('aabbcdd'));
+
+// do not edit below this line
+module.exports = firstNonRepeatedChar;
